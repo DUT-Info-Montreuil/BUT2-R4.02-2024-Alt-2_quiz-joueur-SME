462d88d929db9a54bf9a1f031cb4c67628198e31
ame02h08 <amelia.butinfo@gmail.com>
2024-05-21 17:08:42 +0200

    Ajout PseudoExistantException

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/utils/exceptions/PseudoExistantException.java
@@ -0,0 +1,10 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions;
+
+public class PseudoExistantException extends Exception {
+    public PseudoExistantException() {
+    }
+
+    public PseudoExistantException(String message) {
+        super(message);
+    }
+}
