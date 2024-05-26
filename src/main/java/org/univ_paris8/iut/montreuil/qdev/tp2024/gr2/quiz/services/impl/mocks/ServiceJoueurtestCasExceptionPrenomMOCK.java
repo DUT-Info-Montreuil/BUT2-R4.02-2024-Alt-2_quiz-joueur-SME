695eae1e2c237c3ab705ed7e7302274c9bbeef34
zhak5388 <102757175+zhak5388@users.noreply.github.com>
2024-05-26 21:59:23 +0200

    tests(ajoutJoueur) : Tests unitaires pour la fonction ajoutJoueur (+squash)

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/services/impl/mocks/ServiceJoueurtestCasExceptionPrenomMOCK.java
@@ -0,0 +1,22 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.impl.mocks;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.dto.JoueurDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.enums.LangueEnum;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.models.IServiceJoueur;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.ParametresCreationJoueurException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.PrenomInvalideException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.PseudoExistantException;
+
+import java.util.List;
+
+public class ServiceJoueurtestCasExceptionPrenomMOCK implements IServiceJoueur {
+    @Override
+    public JoueurDTO ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee) throws ParametresCreationJoueurException, PseudoExistantException {
+        throw new PrenomInvalideException();
+    }
+
+    @Override
+    public JoueurDTO ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee, List<String> centreInterets) throws ParametresCreationJoueurException, PseudoExistantException {
+        throw new PrenomInvalideException();
+    }
+}
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/services/impl/mocks/ServiceJoueurtestCasExceptionPseudoExistantMOCK.java
@@ -0,0 +1,37 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.impl.mocks;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.dto.JoueurDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.dto.ScoreDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.enums.LangueEnum;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.models.IServiceJoueur;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.ParametresCreationJoueurException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.PseudoExistantException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceJoueurtestCasExceptionPseudoExistantMOCK implements IServiceJoueur {
+    private List<String> listePseudo;
+
+    public ServiceJoueurtestCasExceptionPseudoExistantMOCK() {
+        this.listePseudo = new ArrayList<>();
+    }
+
+    @Override
+    public JoueurDTO ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee) throws ParametresCreationJoueurException, PseudoExistantException {
+        List<String> centreInterets;
+        centreInterets = new ArrayList<>();
+        return ajouterJoueur(prenom, pseudo, dateNaissance, languePreferee, centreInterets);
+    }
+
+    @Override
+    public JoueurDTO ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee, List<String> centreInterets) throws ParametresCreationJoueurException, PseudoExistantException {
+        if (listePseudo.contains(pseudo)) {
+            throw new PseudoExistantException();
+        } else {
+            listePseudo.add(pseudo);
+            List<ScoreDTO> scores = new ArrayList<>();
+            return new JoueurDTO(prenom, pseudo, dateNaissance, centreInterets, languePreferee, scores);
+        }
+    }
+}
