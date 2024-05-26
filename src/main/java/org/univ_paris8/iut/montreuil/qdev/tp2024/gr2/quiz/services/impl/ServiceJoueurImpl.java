d5fb0356ee22c3a813ae61002df5ffe9ae07c361
ame02h08 <amelia.butinfo@gmail.com>
2024-05-26 16:31:15 +0200

    v1 implémentation IServiceJoueur (méthode ajouterJoueur)

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/services/impl/ServiceJoueurImpl.java
@@ -0,0 +1,94 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.impl;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.dto.JoueurDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.dto.ScoreDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.enums.LangueEnum;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.models.IServiceJoueur;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.ParametresCreationJoueurException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.PseudoExistantException;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class ServiceJoueurImpl implements IServiceJoueur {
+
+    private List<JoueurDTO> lesJoueurs = new ArrayList<>();
+
+    //variable static pour contrôler l'unicité
+    private static ServiceJoueurImpl uniqueInstance;
+
+    //constructeur private pour initialiser les attributs de la classe
+    private ServiceJoueurImpl() {}
+
+    // Methode pour créer l'unique instance du Service Joueur
+    public static ServiceJoueurImpl getUniqueInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new ServiceJoueurImpl();
+        }
+        return uniqueInstance;
+    }
+
+
+    //MÉTHODES
+    @Override
+    public void ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee) throws ParametresCreationJoueurException, PseudoExistantException {
+        ajouterJoueur(prenom, pseudo, dateNaissance, languePreferee, new ArrayList<>());
+    }
+
+    @Override
+    public void ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee, List<String> centreInterets) throws ParametresCreationJoueurException, PseudoExistantException {
+        // Vérification des paramètres
+        validerParametres(prenom, pseudo, dateNaissance, centreInterets);
+        // Vérification de la contrainte du pseudo unique
+        if (siJoueurExiste(pseudo)) {
+            throw new PseudoExistantException("Le pseudo existe déjà.");
+        }
+        // Ajout du joueur
+        JoueurDTO nouveauJoueur = new JoueurDTO(prenom, pseudo, dateNaissance, centreInterets, languePreferee, new ArrayList<ScoreDTO>());
+        lesJoueurs.add(nouveauJoueur);
+    }
+
+
+    //MÉTHODES PRIVATE
+    private void validerParametres(String prenom, String pseudo, int dateNaissance, List<String> centresInterets) throws ParametresCreationJoueurException {
+        if (prenom == null || prenom.length() < 2 || !prenom.matches("[a-zA-Z-'À-ÖØ-öø-ÿ]+")) {
+            throw new ParametresCreationJoueurException("Prénom invalide : doit avoir au moins 2 caractères et ne contenir que des lettres, des tirets ou des apostrophes.");
+        }
+
+        if (pseudo == null || pseudo.length() < 2 || !pseudo.matches("[a-zA-Z0-9-_]+")) {
+            throw new ParametresCreationJoueurException("Pseudo invalide : doit avoir au moins 2 caractères et ne contenir que des lettres, des chiffres, des tirets ou des underscores.");
+        }
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        if (dateNaissance < 1907 || dateNaissance > currentYear) {
+            throw new ParametresCreationJoueurException("Date de naissance invalide : doit être entre 1907 et l'année actuelle.");
+        }
+
+        for (String interet : centresInterets) {
+            if (interet.length() < 2 || !interet.matches("[a-zA-ZÀ-ÖØ-öø-ÿ]+")) {
+                throw new ParametresCreationJoueurException("Centre d'intérêt invalide : chaque intérêt doit avoir au moins 2 caractères et ne contenir que des lettres des cinq alphabets définis.");
+            }
+        }
+
+    }
+
+    private boolean siJoueurExiste(String pseudo) {
+        for (JoueurDTO joueur : lesJoueurs) {
+            if (joueur.getPseudo().equalsIgnoreCase(pseudo)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //GETTERS & SETTERS
+    public List<JoueurDTO> getLesJoueurs() {
+        return lesJoueurs;
+    }
+
+    public void setLesJoueurs(List<JoueurDTO> lesJoueurs) {
+        this.lesJoueurs = lesJoueurs;
+    }
+}
