7bef39f4f5e668013666bdfe1375d13de87d4321
zhak5388 <102757175+zhak5388@users.noreply.github.com>
2024-05-31 18:09:04 +0200

    FIX : changement signature ajouterJoueur

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/services/models/IServiceJoueur.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/services/models/IServiceJoueur.java
@@ -1,5 +1,6 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.services.models;
 
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.dto.JoueurDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.enums.LangueEnum;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.ParametresCreationJoueurException;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.utils.exceptions.PseudoExistantException;
@@ -9,8 +10,8 @@ import java.util.List;
 public interface IServiceJoueur {
 
     //ajouter un joueur sans ses centres d’intérêts
-    void ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee) throws ParametresCreationJoueurException, PseudoExistantException;
+    JoueurDTO ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee) throws ParametresCreationJoueurException, PseudoExistantException;
 
     //ajouter un joueur avec les centres d’intérêts
-    void ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee, List<String> centreInterets) throws ParametresCreationJoueurException, PseudoExistantException;
+    JoueurDTO ajouterJoueur(String prenom, String pseudo, int dateNaissance, LangueEnum languePreferee, List<String> centreInterets) throws ParametresCreationJoueurException, PseudoExistantException;
 }
